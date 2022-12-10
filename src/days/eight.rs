ba1f42f42395ec05474eeee49d870797d306cb34
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2022-12-10 23:23:30 +0100

    revert

--- src/days/eight.rs
+++ src/days/eight.rs
@@ -2,8 +2,7 @@ use std::fs;
 
 pub fn treetop_tree_house() {
     println!("Running day 8");
-    let contents = fs::read_to_string("sample_files/08/example.txt").unwrap();
-    // (height, visible, view distance to [top, left, bottom, right])
+    let contents = fs::read_to_string("sample_files/08/sample.txt").unwrap();
     let mut grid: Vec<Vec<(u32, bool, [u32; 4])>> = Vec::from(Vec::new());
 
     for line in contents.lines() {
@@ -14,8 +13,8 @@ pub fn treetop_tree_house() {
         grid.push(row);
     }
 
-    let n_rows = grid.len();
-    let n_columns = grid.first().unwrap().len();
+    let n_rows = grid.len() - 1;
+    let n_columns = grid.first().unwrap().len() - 1;
 
     let mut max_left;
     let mut max_bottom = Vec::from_iter(
@@ -25,16 +24,9 @@ pub fn treetop_tree_house() {
             .cloned()
             .map(|(height, _, _)| height),
     );
-
-    let mut view_left: (u32, u32);
-    let mut view_bottom = Vec::new();
-    view_bottom.resize(n_columns, (10, 0));
-    println!("{:?}", view_bottom);
-
-    // Check visibility from left and bottom
+    // Check visibility
     for (i, row) in grid.iter_mut().enumerate() {
         max_left = row.first().unwrap().0;
-        view_left = (10, 0);
         for (j, (height, visible, view_distance)) in row.iter_mut().enumerate() {
             // Mark border of forest
             if i == 0 || i == n_rows || j == 0 || j == n_columns {
@@ -45,28 +37,12 @@ pub fn treetop_tree_house() {
             if *height > max_left {
                 *visible = true;
                 max_left = *height;
-            } else if *height >= view_left.0 {
-                view_left.0 = *height;
-                view_left.1 += 1;
-                view_distance[1] = view_left.1;
-            } else {
-                view_left.0 = *height;
-                view_left.1 = 0;
-                view_distance[1] = 0;
             }
 
             // Update visibility from bottom
             if *height > *max_bottom.get(j).unwrap() {
                 *visible = true;
                 *max_bottom.get_mut(j).unwrap() = *height;
-            } else if *height >= view_bottom.get(j).unwrap().0 {
-                view_bottom.get_mut(j).unwrap().0 = *height;
-                view_bottom.get_mut(j).unwrap().1 += 1;
-                view_distance[2] = view_bottom.get(j).unwrap().1;
-            } else {
-                view_bottom.get_mut(j).unwrap().0 = *height;
-                view_bottom.get_mut(j).unwrap().1 = 0;
-                view_distance[2] = 0;
             }
         }
     }
@@ -80,7 +56,7 @@ pub fn treetop_tree_house() {
             .cloned()
             .map(|(height, _, _)| height),
     );
-    // Check visibility from right an top
+    // Check visibility
     for (i, row) in grid.iter_mut().rev().enumerate() {
         println!("{}", i);
         max_right = row.last().unwrap().0;
